44b4b4ae6aeff7f85eb2980d769abd0b13700fbd
Davi Balieiro da Costa <davi.costa1912@gmail.com>
2024-11-16 23:23:42 -0300

    novos atualizacoes

--- Python/Aulas/Intermediario/aula179.py
+++
@@ -1,7 +0,0 @@
-# Funcao recursivas e recursividade
-# Funcao que podem se chamar de volta
-# uteis p/ dividie problemas grandes em partes menores Toda funcao recursiva deve ter:
-# Um problema que posso ser dividido em parte menores
-# Um caso recursivo que resolve o pequeno problema
-# Um caso base que para a recursao
-# Fatorial - n! = 5 * 4 * 3 * 2 * 1 = 120
